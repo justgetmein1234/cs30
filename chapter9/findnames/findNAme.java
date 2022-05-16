45e2f5e393246bf111142200ed2f9b6b01de988f
justgetmein1234 <100795345+justgetmein1234@users.noreply.github.com>
2022-05-16 10:48:44 -0600

    Add files via upload

---
+++ chapter9/findnames/findNAme.java
@@ -0,0 +1,33 @@
+package findnames;
+
+public class findNAme.java
+
+import java.util.Scanner;
+import java.lang.String;
+public class findNAme {
+
+    public static void main(String[] args) {
+        final int MAX = 5;
+        String[] nameArray = new String[MAX];
+        Scanner input = new Scanner(System.in);
+        int name, location;
+
+
+        for (int i = 0; i < nameArray.length; i++) {
+            nameArray[i] = ((MAX + 1) * Math.random());
+        }
+
+
+        System.out.print("Enter a name: ");
+        nameArray[MAX] = input.nextLine();
+
+
+        location = Search.linear(nameArray, name);
+        System.out.println("Enter a name to find: ");
+        location = input.nextLine();
+        if ((location != nameArray[MAX]))
+            System.out.println("Sorry, name cant find. ");
+        else
+            System.out.println(" " + location);
+    }
+}
---
+++ chapter9/highgrade/HIgestGRade.java
@@ -0,0 +1,31 @@
+package highgrade;
+import roasterss.ArrayList;
+import roasterss.Scanner;
+
+public class  HIgestGRade {
+
+	public static void main(String[] args)
+		Scanner scan =  new Scanner(System.in);
+		ArrayList<Integer> scores = new ArrayList<Integer>();
+		System.out.println("ENTER 5 GRADES FROM 0 TO 100.");
+		int greatest = -1;
+		int i = 5;
+		while(scores.size()<5){
+			
+			int input = scan.nextInt();
+			if(input <= 100 && input >= 0){
+				scores.add(input);
+				if(input >= greatest)
+					greatest = input;
+				i--;
+				if(i != 0)
+					System.out.println("ENTER "+i+" MORE GRADES.");
+			}
+			else{
+				System.out.println("Error: MAKE SURE THE grade is FROM 0 and 100!\nEnter a new grade!");
+			}
+		}
+		System.out.println("\nHighest grade: "+greatest);
+		
+	}
+}
